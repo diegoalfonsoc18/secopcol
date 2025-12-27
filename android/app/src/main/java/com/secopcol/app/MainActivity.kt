82980b58e86c7b5dbe68c636b47064e00ae0fd94
Diego Alfonso <dlalfonso57@gmail.com>
2025-12-27 12:00:53 -0500

    feat: update

--- android/app/src/main/java/com/secopcol/app/MainActivity.kt
+++ android/app/src/main/java/com/secopcol/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.secopcol
+package com.secopcol.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/secopcol/app/MainApplication.kt
+++ android/app/src/main/java/com/secopcol/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.secopcol
+package com.secopcol.app
 
 import android.app.Application
 import android.content.res.Configuration
